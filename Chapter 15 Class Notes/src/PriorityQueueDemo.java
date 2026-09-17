0be48f01f9ebf7e47d5d83c6c5e8b573e68000f2
rina-xu <rjxu@stu.naperville203.org>
2026-09-17 15:08:04 -0500

    notes

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,20 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        // Create a priority queue of strings
+        // a priority queue MUSTbe composed of Comparable objects
+        Queue<String> students = new PriorityQueue<>();
+        students.add("Shiva");
+        students.add("Claire");
+        students.add("Ludovic");
+        students.add("Manny");
+        students.add("Ethan");
+        students.add("Davin");
+
+        // The next highest priority object is moved to the front of the queue
+        // when the head of the queue is removed
+        while (students.size() > 0) {
+            System.out.println(students.remove());
+        }
     }
 }
