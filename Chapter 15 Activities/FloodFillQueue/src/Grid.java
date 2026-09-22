b06ccae99224b6efff6d3f9da2dce15463e9cf54
rina-xu <rjxu@stu.naperville203.org>
2026-09-22 15:10:39 -0500

    progress 9/22

--- Chapter 15 Activities/FloodFillQueue/src/Grid.java
+++ Chapter 15 Activities/FloodFillQueue/src/Grid.java
@@ -1,16 +1,34 @@
-. . .
+import java.util.*;
 public class Grid
 {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    . . .
+    Stack<Pair> stack = new Stack<Pair>();
+    Pair pair;
 
     /**
      * Flood fill, starting with the given row and column.
     */
     public void floodfill(int row, int column)
     {
-        . . .
+        pair = new Pair(row, column);
+        int num = 1;
+        stack.push(pair);
+        while (!stack.isEmpty()) {
+            for (int i = row; i < SIZE - row; i++)
+            {
+                for (int j = column; j < SIZE - column; j++)
+                {
+                    if (pixels[i][j] == 0)
+                    {
+                        pixels[i][j] = num;
+                        pair = new Pair(i, j);
+                        stack.push(pair);
+                    }
+                }
+            }
+        }
+        
     }
 
     public String toString()
